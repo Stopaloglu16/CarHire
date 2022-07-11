4c10467b53de15659329904d2cc7a4922f79d47d
Sertac Topaloglu <sertac.topaloglu@hotmail.co.uk>
2022-07-11 22:14:48 +0100

    car brand test added

---
+++ Tests/Application.IntegrationTests/TestTemp/Commands/ad.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.IntegrationTests.TestTemp.Commands
+{
+    internal class ad
+    {
+    }
+}
